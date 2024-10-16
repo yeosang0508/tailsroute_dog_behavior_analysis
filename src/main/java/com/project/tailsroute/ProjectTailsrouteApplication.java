986a19d6c2107f924fe12c8c06cc2e5728f068e7
jby9725 <jby9725@gmail.com>
2024-10-16 13:03:06 +0900

    정리

--- src/main/java/com/project/tailsroute/ProjectTailsrouteApplication.java
+++ src/main/java/com/project/tailsroute/ProjectTailsrouteApplication.java
@@ -8,10 +8,6 @@ public class ProjectTailsrouteApplication {
 
 	public static void main(String[] args) {
 		System.out.println("안녕하세요!!!");
-		System.out.println("안녕하세요");
-		System.out.println("안녕하세요");
-		System.out.println("안녕하세요 인사빌런이에요");
-		System.out.println("제발좀");
 		SpringApplication.run(ProjectTailsrouteApplication.class, args);
 	}
 }
