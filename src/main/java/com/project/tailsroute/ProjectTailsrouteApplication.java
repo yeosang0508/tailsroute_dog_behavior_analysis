fc9ca0d949fb41a90d9469513b0b9010b2b5405a
Eichi2514 <picon1317@gmail.com>
2024-10-16 17:23:50 +0900

    test2

--- src/main/java/com/project/tailsroute/ProjectTailsrouteApplication.java
+++ src/main/java/com/project/tailsroute/ProjectTailsrouteApplication.java
@@ -9,7 +9,7 @@ public class ProjectTailsrouteApplication {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요!!!");
 		System.out.println("안녕하세요");
-		System.out.println("안녕하세요");
+		System.out.println("안녕하세요   ");
 		SpringApplication.run(ProjectTailsrouteApplication.class, args);
 	}
 }
