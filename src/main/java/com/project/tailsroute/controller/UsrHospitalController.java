35c07fe13cd9076a7a3348e25bfd318228d9ea46
jby9725 <jby9725@gmail.com>
2024-10-17 09:55:48 +0900

    JBY : GOOGLE_MAP_API 키 가리고 dotenv 라이브러리로 불러와 기존 기능 사용하기

--- src/main/java/com/project/tailsroute/controller/UsrHospitalController.java
+++ src/main/java/com/project/tailsroute/controller/UsrHospitalController.java
@@ -1,14 +1,22 @@
 package com.project.tailsroute.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UsrHospitalController {
+
+    // 루트 디렉토리에 있는 .env 파일에서 API 키를 불러옴.
+    @Value("${GOOGLE_MAP_API_KEY}")
+    private String API_KEY;
+
     @GetMapping("/usr/hospital/main")
     public String showMain(Model model) {
+
+        model.addAttribute("GOOGLE_MAP_API_KEY", API_KEY);
         model.addAttribute("message", "24시간 병원 message 추가");
         return "usr/map/hospital";
     }
